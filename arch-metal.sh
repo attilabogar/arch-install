b19ba7829a9e3b183efd3f082d166f23c14fb114
Attila Bogár <attila.bogar@gmail.com>
2019-10-02 03:08:54 +0100

    arch-metal.sh: completely rewritten

--- arch-metal.sh
+++ arch-metal.sh
@@ -13,12 +13,13 @@ export FQDN=${FQDN:-arch.example.com}
 export LANG=${LANG:-en_GB.UTF-8}
 export TZ=${TZ:-Europe/London}
 export ROOTPW=${ROOTPW:-root}
+export UNLOCK=${UNLOCK:-none}
+# size in GiB or 100% (default)
+export SIZE=${SIZE:-100%}
+[[ $SIZE == "100%" ]] || SIZE=$[SIZE*1024*1024*2-1]s
 
 # you must explicitly set the UNLOCK environment variable on invocation
-# this fails if UNLOCK is unset
-# special value none means no encryption
-
-LUKS_PHRASE="$UNLOCK"
+# value none means no encryption will be applied
 
 if ! [ -b "$DISK" ]
 then
@@ -26,11 +27,42 @@ then
   exit 1
 fi
 
-export ROTATIONAL=$(cat /sys/block/${DISK##/dev/}/queue/rotational)
+HAVE_EFI=0
+[[ -d /sys/firmware/efi ]] && HAVE_EFI=1
+HAVE_TRIM=$(lsblk -D -l | grep ^${DISK##/dev/} | awk '{print $3}')
+if [[ $HAVE_TRIM == "0B" ]]
+then
+  HAVE_TRIM=0
+else
+  HAVE_TRIM=1
+fi
+HAVE_HDD=0
+[[ $HAVE_TRIM == 0 ]] && HAVE_HDD=$(cat /sys/block/${DISK##/dev/}/queue/rotational)
+HAVE_INTEL=0
+grep -q Intel /proc/cpuinfo && HAVE_INTEL=1
+HAVE_WIFI=0
+rfkill|grep -q wlan && HAVE_WIFI=1
+HAVE_BT=0
+rfkill|grep -q bluetooth && HAVE_BT=1
+HAVE_CRYPT=1
+[[ "$UNLOCK" == "none" ]] && HAVE_CRYPT=0
+NODE=$(cut -d. -f1 <<< $FQDN)
 
 # set up country specific source
 MIRRORLIST="https://www.archlinux.org/mirrorlist/?country=${COUNTRY}&protocol=https&ip_version=4&use_mirror_status=on"
 curl -s "$MIRRORLIST" | sed 's/^#Server/Server/' > /etc/pacman.d/mirrorlist
+
+# set up required packages
+pacman -Syy
+PACKAGES="$(pacman -Sqg base | sed 's/^linux$/&-lts/') linux-lts-headers base-devel"
+PACKAGES="$PACKAGES openssh python2 bridge-utils ethtool vim dmidecode tcpdump bc rsync net-tools parted"
+[[ $HAVE_EFI == 0 ]] && PACKAGES="$PACKAGES grub"
+[[ $HAVE_EFI == 1 ]] && PACKAGES="$PACKAGES efibootmgr"
+[[ $HAVE_HDD == 0 ]] && PACKAGES="$PACKAGES f2fs-tools"
+[[ $HAVE_INTEL == 1 ]] && PACKAGES="$PACKAGES intel-ucode"
+[[ $HAVE_INTEL == 0 ]] && PACKAGES="$PACKAGES amd-ucode"
+[[ $HAVE_WIFI == 1 ]] && PACKAGES="$PACKAGES rfkill dialog wpa_supplicant crda"
+[[ $HAVE_BT == 1 ]] && PACKAGES="$PACKAGES bluez bluez-utils"
 
 # wipe off existing systems
 for part in `grep ${DISK##/dev/}$ /proc/partitions|sort -r|awk '{print $4}'`
@@ -39,88 +71,83 @@ do
   wipefs -f -a /dev/$part
 done
 
-# ready to go
-if [ -d /sys/firmware/efi ]
+# partitioning
+if [[ $HAVE_EFI == 1 ]]
 then
   # EFI
+  LABEL=rootfs
+  [[ $HAVE_CRYPT == 1 ]] && LABEL=encrypted
   parted -s $DISK mklabel gpt
   parted -s $DISK mkpart ESP fat32 2048s $[1024*2*512-1]s
-  if [ "$UNLOCK" = "none" ]; then
-    parted -s $DISK mkpart rootfs xfs $[1024*2*512]s 100%
-  else
-    parted -s $DISK mkpart encrypted xfs $[1024*2*512]s 100%
-  fi
+  parted -s $DISK mkpart $LABEL xfs $[1024*2*512]s "$SIZE"
 else
   # BIOS
   parted -s $DISK mklabel msdos
   parted -s $DISK mkpart primary xfs 2048s $[1024*2*512-1]s
   parted -s $DISK mkpart primary xfs $[1024*2*512]s 100%
 fi
+
 parted -s $DISK set 1 boot on
 
 # wait until kernel refreshes the partitions
 while ! [ -b ${DISK}1 ] ; do sleep 1 ; done
 while ! [ -b ${DISK}2 ] ; do sleep 1 ; done
 
-if [ -d /sys/firmware/efi ]
+# CREATE BOOT FILESYSTEM
+if [[ $HAVE_EFI == 1 ]]
 then
   # EFI
-  mkfs.fat -F32 -n ESP ${DISK}1
+  mkfs.fat -v -F32 -n ESP ${DISK}1
 else
   # BIOS
-  mkfs.xfs -L boot ${DISK}1
+  mkfs.xfs -f -L $NODE-boot ${DISK}1
 fi
 
-if [ "$UNLOCK" = "none" ]
+# CREATE LUKS DEVICE IF NEEDED
+if [[ $HAVE_CRYPT == 1 ]]
 then
-  ROOT_PARTITION="${DISK}2"
-  # don't encrypt root partition
-  mkfs.xfs -f -L rootfs "$ROOT_PARTITION"
-  # get the UUID
-  eval $(blkid "$ROOT_PARTITION" | cut -d' ' -f3)
-  ROOT_UUID=$UUID
-else
-  # setup LUKS key
   LUKS_KEY="$(mktemp)"
-  echo -n "$LUKS_PHRASE" > "$LUKS_KEY"
-  
-  # create LUKS
-  cryptsetup --batch-mode --key-file=$LUKS_KEY -c aes-xts-plain64 \
-    -s 512 luksFormat ${DISK}2
-  
-  # get LUKS UUID
-  eval $(blkid ${DISK}2 | cut -d' ' -f2)
+  echo -n "$UNLOCK" > "$LUKS_KEY"
+  cryptsetup --type luks1 --batch-mode --key-file=$LUKS_KEY -c aes-xts-plain64 \
+    -s 512 --hash sha512 --iter-time 5000 --use-random luksFormat ${DISK}2
+  eval $(blkid -o export ${DISK}2)
   LUKS_UUID=$UUID
-  echo "LUKS_UUID = $LUKS_UUID"
-  
-  # open LUKS
-  cryptsetup --batch-mode --key-file=$LUKS_KEY luksOpen ${DISK}2 cryptroot
-  
-  # create xfs root file system
-  mkfs.xfs -f -L cryptroot /dev/mapper/cryptroot
-  ROOT_PARTITION="/dev/mapper/cryptroot"
+  if [[ $HAVE_TRIM == 1 ]]
+  then
+    cryptsetup --batch-mode --key-file=$LUKS_KEY --allow-discards luksOpen ${DISK}2 cryptroot
+  else
+    cryptsetup --batch-mode --key-file=$LUKS_KEY luksOpen ${DISK}2 cryptroot
+  fi
+  ROOTDEV=/dev/mapper/cryptroot
+  LUKS_OPTS=""
+  [[ $HAVE_TRIM == 1 ]] && LUKS_OPTS=":allow-discards"
+else
+  ROOTDEV=${DISK}2
 fi
+
+# CREATE ROOT FILESYSTEM
+if [[ $HAVE_HDD == 1 ]]
+then
+  mkfs.xfs -f -L $NODE-rootfs $ROOTDEV
+else
+  mkfs.f2fs -f -l $NODE-rootfs $ROOTDEV
+fi
+
+MOUNT_OPTS="defaults"
+[[ $HAVE_TRIM == 1 ]] && MOUNT_OPTS="$MOUNT_OPTS,discard"
 
 # mount rootfs
-if [[ $ROTATIONAL == 1 ]] 
-then
-  mount "$ROOT_PARTITION" /mnt
-else
-  mount "$ROOT_PARTITION" /mnt -o discard
-fi
+eval $(blkid -o export $ROOTDEV)
+ROOT_UUID=$UUID
+mount "$ROOTDEV" /mnt -o $MOUNT_OPTS
 
 # mount boot
-mkdir /mnt/boot
-if [ $ROTATIONAL = 0 -a ! -d /sys/firmware/efi ]
-then
-  mount ${DISK}1 /mnt/boot -o discard
-else
-  mount ${DISK}1 /mnt/boot
-fi
+mkdir -p /mnt/boot
+mount ${DISK}1 /mnt/boot -o $MOUNT_OPTS
 
 # bootstrap
 pacman -Syy
-pacstrap /mnt $(pacman -Sqg base | sed 's/^linux$/&-lts/') base-devel
+pacstrap /mnt $PACKAGES
 
 # fstab
 genfstab -U -p /mnt >> /mnt/etc/fstab
@@ -136,115 +163,80 @@ echo "KEYMAP=$KEYMAP" > /mnt/etc/vconsole.conf
 echo "$LANG UTF-8" >> /mnt/etc/locale.gen
 echo "LANG=$LANG" > /mnt/etc/locale.conf
 
-if ! [ "$UNLOCK" = "none" ]
+if [[ $HAVE_CRYPT == 1 ]]
 then
   # fix hooks
   cp -p /mnt/etc/mkinitcpio.conf /mnt/etc/mkinitcpio.conf.pacnew
   sed -i -e 's,^HOOKS=.*$,HOOKS="base udev autodetect modconf block keyboard keymap encrypt filesystems fsck",' \
     /mnt/etc/mkinitcpio.conf
-
-  # LUKS on SSD?
-  export LUKS_OPTS=""
-  [ $ROTATIONAL = 0 ] && LUKS_OPTS=":allow-discards"
 fi
 
 # root password
 echo root:$ROOTPW | chpasswd -R /mnt
 
-# set up dhcp if on wired networking
-NETIF="$(gawk '/^en/{if ($2==00000000) { print $1 }}' /proc/net/route)"
-
-if [ -n "$NETIF" ]
-then
-  cat > /mnt/etc/netctl/wired <<EOD
-Description='Bridged Wired Ethernet'
-Interface=br0
-BindsToInterfaces=('$NETIF')
-Connection=bridge
-IP=dhcp
-SkipForwardingDelay=yes
-EOD
-fi
-
-# post setup
-cat > /mnt/root/post.sh << EOD
-#!/bin/bash
-set -euxo pipefail
-
 # generate locale(s)
-locale-gen
-
-# some extra stuff needed for ansible
-pacman --noconfirm -Syy
-pacman --noconfirm -S intel-ucode openssh python2 bridge-utils
-# systemctl enable sshd
-
-# check for wired ethernet
-# if [ -s /etc/netctl/wired ]
-# then
-#   netctl enable wired
-# fi
+arch-chroot /mnt locale-gen
 
 # create initrd
-mkinitcpio -p linux-lts
+arch-chroot /mnt mkinitcpio -p linux-lts
 
-if [ -d /sys/firmware/efi ]
+if [[ $HAVE_EFI == 1 ]]
 then
-  # EFI -- setting entries from outside the chroot 
-  pacman --noconfirm -S efibootmgr
-  bootctl --path=/boot install || \
-    efibootmgr -c -d $DISK -p1 -D -l /EFI/systemd/systemd-bootx64.efi \
-    -L 'Linux Boot Manager' 
+  # clean-up existing efi boot vars
+  efibootmgr | grep 'Linux Boot Manager$' | cut -c5-8 | xargs --no-run-if-empty -n1 efibootmgr -B -b
+  # install systemd boot manager
+  arch-chroot /mnt \
+    bootctl install
 else
   # BIOS
-  pacman --noconfirm -S grub
-  grub-install --target=i386-pc --recheck $DISK
+  arch-chroot /mnt \
+    grub-install --target=i386-pc --recheck $DISK
 fi
-EOD
-chmod +x /mnt/root/post.sh
-arch-chroot /mnt /root/post.sh
 
-if [ -d /sys/firmware/efi ]
+KERNEL_LUKS=""
+[[ $HAVE_CRYPT == 1 ]] && KERNEL_LUKS="cryptdevice=UUID=$LUKS_UUID:cryptroot$LUKS_OPTS"
+
+if [[ $HAVE_EFI == 1 ]]
 then
-
-  # EFI
+  KERNEL_CMDLINE="root=UUID=$ROOT_UUID rw"
+  [[ $HAVE_CRYPT == 1 ]] && KERNEL_CMDLINE="$KERNEL_CMDLINE $KERNEL_LUKS"
+  MICROCODE="intel-ucode.img"
+  [[ $HAVE_INTEL == 0 ]] && MICROCODE="amd-ucode.img"
   cat > /mnt/boot/loader/entries/arch.conf << EOD
 title Arch Linux
 linux /vmlinuz-linux-lts
-initrd /intel-ucode.img
+initrd /$MICROCODE
 initrd /initramfs-linux-lts.img
+options $KERNEL_CMDLINE
 EOD
-
-  if [ "$UNLOCK" = "none" ]
-  then
-    echo "options root=UUID=$ROOT_UUID rw"
-  else
-    echo "options cryptdevice=UUID=$LUKS_UUID:cryptroot$LUKS_OPTS root=/dev/mapper/cryptroot net.ifnames=0 rw"
-  fi >> /mnt/boot/loader/entries/arch.conf
-
 else
-  if ! [ "$UNLOCK" = "none" ]
-  then
-    cp -p /mnt/etc/default/grub /mnt/etc/default/grub.pacnew
-    sed -i -e "s,^GRUB_CMDLINE_LINUX=.*$,GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$LUKS_UUID:cryptroot${LUKS_OPTS} net.ifnames=0\"," /mnt/etc/default/grub
-  fi
-  # generate fresh grub config
+  # BIOS
+  cp -p /mnt/etc/default/grub /mnt/etc/default/grub.pacnew
+  [[ $HAVE_CRYPT == 1 ]] && \
+    sed -i -e "s,^GRUB_CMDLINE_LINUX=.*$,GRUB_CMDLINE_LINUX=\"$KERNEL_LUKS\"," \
+    /mnt/etc/default/grub
   arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
 # swap
-if [ $ROTATIONAL = 0 ]
+if [[ $HAVE_HDD == 0 ]]
 then
-  echo 'vm.swappiness = 0' > /mnt/etc/sysctl.d/swap-off.conf
+  echo 'vm.swappiness = 0' > /mnt/etc/sysctl.d/no-swap.conf
 else
   dd if=/dev/zero of=/mnt/swapfile bs=1024k count=1024
   chmod 600 /mnt/swapfile
   mkswap /mnt/swapfile
-  echo '# swap' >> /mnt/etc/fstab
+  echo  >> /mnt/etc/fstab
+  echo '# swap on file' >> /mnt/etc/fstab
   echo '/swapfile none swap defaults 0 0' >> /mnt/etc/fstab
 fi
+
+# auto-cleaning pacman cache
+find /mnt/var/cache/pacman/pkg/ -type f -delete
+curl -R -L -o /mnt/usr/share/libalpm/hooks/package-cleanup.hook \
+  https://raw.githubusercontent.com/archlinux/archlinux-docker/master/rootfs/usr/share/libalpm/hooks/package-cleanup.hook
 
 # sync file systems
 sync
 
-echo "NOTE: check for network setup and enable"
+echo "NOTE: setup networking if needed"
